9e00cffcbcadd8162673b3bd5bd63e81e7546976
Toib Fayaz <108783421+Toib765@users.noreply.github.com>
2026-04-28 14:05:38 +0530

    Delete Project directory

--- Project/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ICC Champions Trophy 2025 Dashboard
-if not exist champions_trophy.db (
-    echo Setting up database...
-    python init_db.py
-)
-echo Starting server at http://localhost:5000
-python app.py
-pause
